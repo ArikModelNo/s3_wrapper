bb79b6deb2e5022970efb30d91f34d090f2f6519
Arik Rosenthal <arik@model-no.com>
2019-10-24 15:08:21 -0700

    Create examples

---
+++ examples.py
@@ -0,0 +1,15 @@
+from s3_class import S3
+
+
+# Create an S3 object and set current workind directory if desired
+s = S3('ED')
+
+# Upload file located at URL to desired directory
+s.uploadWithURL('production_files', data_url)
+
+# Upload base64 encoded data to desired directory
+s.uploadWithBase64('screenshots', 'example_base64.png', data_base64)
+
+# Upload local file to desired directory
+with open('/path/to/file', 'rb') as data_binary:
+    s.uploadWithBinary('screenshots', 'example_binary.png', data_binary)

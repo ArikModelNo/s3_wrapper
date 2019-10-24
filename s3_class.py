1ef88821d9ec53206d185caa6ccd87ce7e56a667
Arik Rosenthal <arik@model-no.com>
2019-10-24 15:33:10 -0700

    Create S3 Class

---
+++ s3_class.py
@@ -0,0 +1,141 @@
+import operator
+import requests
+import base64
+import boto3
+import re
+from s3_config import config
+
+
+class S3():
+	"""
+	Class for interacting with an AWS S3 Bucket.
+
+	Make sure to create s3_config.py and populate it.
+	"""
+
+	def __init__(self, cwd = ''):
+		self.cwd = cwd
+		self._session = boto3.Session(
+			aws_access_key_id=config['AWS_ACCESS_KEY_ID'],
+			aws_secret_access_key=config['AWS_SECRET_ACCESS_KEY'])
+		self._bucket = self._session.resource('s3').Bucket(config['BUCKET'])
+
+	cwd = property(operator.attrgetter('_cwd'))
+
+	@cwd.setter
+	def cwd(self, c):
+		if c.endswith('/'):
+			c = c[:-1]
+		self._cwd = c
+
+	def printCWD(self):
+		"""
+		Prints the current working directory.
+		
+		Returns nothing.
+		"""
+		print(self.cwd)
+
+	def _assemblePath(self, path, filename):
+		"""
+		Assembles a full path from given parameters.
+
+		Returns str of full path.
+		"""
+		if self.cwd == '':
+			return f'{path}/{filename}'
+		else:
+			return f'{self.cwd}/{path}/{filename}'
+
+	def accessibleFiles(self, **kwargs):
+		"""
+		Determines which files are accessible from CWD.
+		Optionally prints list when called as accessibleFiles(print=True)
+
+		Returns list of accessible files.
+		"""
+		accessible_files = []
+		if kwargs.get('print') is True:
+			print('** Accessible Files **')
+		for path in self._bucket.objects.all():
+			if path.key.startswith(self.cwd):
+				if not path.key.endswith('/'):
+					accessible_files.append(path.key)
+					if kwargs.get('print') is True:
+						print('\t' + path.key)
+		return accessible_files
+
+	def accessibleDirectories(self):
+		"""
+		Determines which directories are accessible from CWD
+		and prints the hierarchy.
+
+		Returns nothing.
+		"""
+		print('** Accessible Directories **')
+		for path in self._bucket.objects.all():
+			if path.key.startswith(self.cwd):
+				if path.key.endswith('/'):
+					list_of_path = path.key.split('/')
+					print('\t' * (len(list_of_path) - 1), end='')
+					print(list_of_path[-2])
+
+	def checkIfFileExists(self, path, filename):
+		"""
+		Checks if a file at a given path exists.
+		Prints full path to file if found.
+
+		Returns True if it does, False if it doesn't.
+		"""
+		full_path = self._assemblePath(path, filename)
+		for obj_path in self._bucket.objects.all():
+			if full_path == obj_path.key:
+				print(f'Found file [{full_path}]!')
+				return True
+		return False
+
+	def uploadWithBase64(self, path, filename, data_base64):
+		"""
+		Uploads file of given base64 data.
+
+		Returns nothing.
+		"""
+		data_base64 = re.sub('.*base64,', '', data_base64)
+		data_base64 = base64.b64decode(data_base64)
+		full_path = self._assemblePath(path, filename)
+		self._bucket.put_object(Key=full_path, Body=data_base64)
+
+	def uploadWithBinary(self, path, filename, data_binary):
+		"""
+		Uploads file of given binary.
+
+		Returns nothing.
+		"""
+		full_path = self._assemblePath(path, filename)
+		self._bucket.put_object(Key=full_path, Body=data_binary)
+
+	def uploadWithURL(self, path, data_url):
+		"""
+		Uploads file of given url.
+
+		Returns False upon failure to download or interprate file.
+		"""
+		try:
+			response = requests.get(data_url)
+		except:
+			print('Failed to retrieve file')
+			return False
+		data_binary = response.content
+		content_disposition = response.headers.get('Content-Disposition')
+		if content_disposition is None:
+			print('Failed to retrieve content disposition')
+			return False
+		else:
+			regex_result = re.findall('filename="(.*?)"', content_disposition)[0]
+			if regex_result != '':
+				filename = regex_result
+			else:
+				print('Failed to retrieve file name')
+				filename = 'untitled'
+		full_path = self._assemblePath(path, filename)
+		self._bucket.put_object(Key=full_path, Body=data_binary)

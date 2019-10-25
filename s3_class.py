cc244ae0d7f154bfea71db93c16d8e443c79b48a
Arik Rosenthal <arik@model-no.com>
2019-10-25 14:22:40 -0700

    Update with path to module to allow importing when used out of dir

--- s3_class.py
+++ s3_class.py
@@ -3,7 +3,12 @@ import requests
 import base64
 import boto3
 import re
-from s3_config import config
+import os
+import sys
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+from s3_wrapper.s3_config import config
 
 
 class S3():
